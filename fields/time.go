052bdfc88ab1eeaf88bedd67615008f72d2946bc
Christopher John Jackson <hello@cj-jackson.com>
2016-05-08 09:06:44 +0100

    Possible bug with timezone

--- fields/time.go
+++ fields/time.go
@@ -127,9 +127,9 @@ func (t Time) Transform() {
 func (t Time) ReverseTransform() {
 	norm := strings.TrimSpace(*t.Norm)
 	*t.Model = time.Time{}
+	location := useDefaultIfNotUserDefinedOrCouldntFindIt(t.Location, t.UserLocation)
 	for _, format := range t.Formats {
-		out, err := time.ParseInLocation(format, norm,
-			useDefaultIfNotUserDefinedOrCouldntFindIt(t.Location, t.UserLocation))
+		out, err := time.ParseInLocation(format, norm, location)
 		if nil != err {
 			continue
 		}
