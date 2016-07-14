7a902cdc735a0ab54fb20c9e2a9b720063bd4c4d
Christopher John Jackson <hello@cj-jackson.com>
2016-07-14 20:27:16 +0100

    Added field abstract

---
+++ field_abstract.go
@@ -0,0 +1,81 @@
+package form
+
+type FieldAbstract struct {
+	_type     string
+	multi     bool
+	label     *string
+	name      *string
+	suffix    **string
+	id        string
+	value     *string
+	values    *[]string
+	inputAttr map[string]string
+	checked   bool
+	options   []string
+}
+
+func NewFieldAbstract(
+	_type string,
+	multi bool,
+	label *string,
+	name *string,
+	suffix **string,
+	value *string,
+	values *[]string,
+) *FieldAbstract {
+	return &FieldAbstract{
+		_type:     _type,
+		multi:     multi,
+		label:     label,
+		name:      name,
+		suffix:    suffix,
+		value:     value,
+		values:    values,
+		inputAttr: map[string]string{},
+	}
+}
+
+func (f *FieldAbstract) Id() string {
+	return f.id
+}
+
+func (f *FieldAbstract) Label() string {
+	return *f.label
+}
+
+func (f *FieldAbstract) Name() string {
+	if nil != *f.suffix {
+		return *f.name + **f.suffix
+	}
+
+	return *f.name
+}
+
+func (f *FieldAbstract) Value() string {
+	return *f.value
+}
+
+func (f *FieldAbstract) Values() []string {
+	return *f.value
+}
+
+func (f *FieldAbstract) InputAttr() map[string]string {
+	delete(f.inputAttr, "id")
+	delete(f.inputAttr, "class")
+	delete(f.inputAttr, "selected")
+	delete(f.inputAttr, "checked")
+
+	return f.inputAttr
+}
+
+func (f *FieldAbstract) SetInputAttr(name, value string) {
+	f.inputAttr[name] = value
+}
+
+func (f *FieldAbstract) SetChecked(checked bool) {
+	f.checked = checked
+}
+
+func (f *FieldAbstract) Checked() bool {
+	return f.checked
+}
